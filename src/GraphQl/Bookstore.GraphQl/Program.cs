4d3d798b80f4ab29d08337dba4f3a1fde9a4ee60
Murilo Beltrame <murilobeltrame@gmail.com>
2023-03-20 00:04:43 -0300

    ...

--- src/GraphQl/Bookstore.GraphQl/Program.cs
+++ src/GraphQl/Bookstore.GraphQl/Program.cs
@@ -1,8 +1,13 @@
 using Bookstore.GraphQl;
+using Bookstore.GraphQl.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
+var configuration = builder.Configuration;
+
 // Add services to the container.
+var bookClientConnectionString = configuration.GetConnectionString("books-api");
+builder.Services.AddHttpClient<BooksClient>(o => o.BaseAddress = new Uri(bookClientConnectionString!));
 builder.Services
     .AddGraphQLServer()
     .AddQueryType<Query>();
--- src/GraphQl/Bookstore.GraphQl/Query.cs
+++ src/GraphQl/Bookstore.GraphQl/Query.cs
@@ -1,14 +1,23 @@
-﻿using Bookstore.GraphQl.Types;
+﻿using Bookstore.GraphQl.Services;
 
 namespace Bookstore.GraphQl
 {
     public class Query
 	{
-		public Book GetBook() => new()
+		public Task<ICollection<FetchBookQueryResponse>> GetBooks(
+			[Service] BooksClient client,
+			CancellationToken cancellationToken)
 		{
-			Author = new() { Name = "Jon Skeet" },
-			Title = "C# in depth"
-		};
+			Console.WriteLine($"Calling service based on {client.BaseUrl}");
+			return client.BooksAllAsync(0, 100, string.Empty, string.Empty, cancellationToken);
+		}
+
+		public Task<GetBookQueryResponse> GetBook(
+			[Service] BooksClient client,
+			int id,
+			CancellationToken cancellationToken)
+		{
+			return client.BooksGETAsync(id, cancellationToken);
+		}
 	}
 }
-
